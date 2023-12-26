1545459421dd821a80783f1111f5d3420b4e69e6
chrismcintosh78 <154263252+chrismcintosh78@users.noreply.github.com>
2023-12-26 14:25:12 -0800

    Update config.js

--- models/assets/js/config.js
+++ models/assets/js/config.js
@@ -1,7 +1,33 @@
+/**
+ * Configuration object for the application.
+ *
+ * This object contains various configuration settings for the application,
+ * including paths to directories for CSS, JavaScript, and vendor assets.
+ */
 const CONFIG = {
-    paths: {
-        cssDirPath: "models/assets/css/",
-        jsDirPath:  "models/assets/js/",
-        venDirPath: "models/assets/vendor/",
-    },
-}
+  /**
+   * Paths to directories for assets.
+   */
+  paths: {
+    /**
+     * Path to the CSS directory.
+     * @type {string}
+     */
+    cssDirPath: "models/assets/css/",
+
+    /**
+     * Path to the JavaScript directory.
+     * @type {string}
+     */
+    jsDirPath: "models/assets/js/",
+
+    /**
+     * Path to the vendor directory.
+     * @type {string}
+     */
+    venDirPath: "models/assets/vendor/",
+  },
+};
+
+// Export the CONFIG object
+export { CONFIG };
