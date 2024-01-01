566c19e3fe069c8f444b8275419a232f9d570b4d
Chris McIntosh <cmcintosh.js@gmail.com>
2024-01-01 11:54:15 -0800

    still layouot

--- models/assets/js/mods/layout/header.mod.js
+++ models/assets/js/mods/layout/header.mod.js
@@ -69,7 +69,7 @@ const Header = (function () {
                         $("#divSection_Contact").append($("<a href='mailto:contact@example.com'>info@example.com</a>"))
                         let iPhoneIcon = objUtil.cre8WClass("i", "", "bi bi-phone-fill phone-icon");
                         $("#divSection_Contact").append(iPhoneIcon); 
-                        $("#divSection_Contact").append($("1-559-400-5295"));
+                        $("#divSection_Contact").append("1-559-400-5295");
                     let objSocialDiv = objUtil.cre8WClass("div", "divSection_Social", "social-links d-none d-md-block");
                     $("#divSection").append(objSocialDiv);
                         $("#divSection_Social").append('<a href="#" class="twitter"><i class="bi bi-twitter"></i></a>');
